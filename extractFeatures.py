346d74780af9e96074d6bbb371813cd47a03b7f4
narleras <naroa.zurutuza@gmail.com>
2013-12-02 21:45:36 -0800

    Update extractFeatures.py

--- extractFeatures.py
+++ extractFeatures.py
@@ -4,18 +4,20 @@
 # @ return: 
 #    Pitch features
 #    Number of notes
+import numpy as np
+
 def extractPitch(piece, numVoices):
     features = [0.0] * 12
     numRows, numCols = piece.shape
     numNotes = 0
     for voiceIndex in range(numVoices):
         voice = 5 + 4 * voiceIndex
-        for r in range(numRows + 1):
-            note = (int)piece[r][voice]
+        for r in range(numRows):
+            note = piece[r][voice]
             if note <= 0: continue
-                pitch = note % 12
-                numNotes += 1
-                features[pitch] += 1
+            pitch = (int)(note % 12)
+            numNotes += 1
+            features[pitch] += 1
     for i in range(len(features)):
         features[i] /= numNotes
     return features, numNotes
@@ -58,12 +60,15 @@ def initFeatureVectors(trainingSongs, numComposers, numTrainingPieces):
     print ''
     print 'Extracting features...' 
     numFeatures = 13
-    featureVector = np.zeros(numTrainingPieces, numFeatures)
+    featureVector = np.zeros((numTrainingPieces, numFeatures))
+    composers = np.zeros(numTrainingPieces)
     currPiece = 0
     for composer, piece, numVoices in trainingSongs:
+        composers[currPiece] = (int)(composer)
         features = extractFeatures(piece, numVoices)
         for i in range(numFeatures):
-            featureVector[currPiece][i] = features[i]
+            featureVector[currPiece][i] = (int)(features[i])
+        currPiece += 1
     print 'Finished feature extraction'
     print ''
-    return featureVectors
+    return featureVector, composers
